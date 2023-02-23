835ceba7599f9ae70d5e5cb98071f9241625d4b3
anderrester <anderrester@mail.ru>
2023-02-24 00:13:33 +0200

    HARD UPDATE

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
-var carousel = $(".carousel"),
-  currdeg = 0,
-  num = 1;
+var carousel = $('.carousel'),
+    currdeg = 0,
+    num = 1;
 
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
+$('.next').on('click', { d: 'n' }, rotate);
+$('.prev').on('click', { d: 'p' }, rotate);
 
 function rotate(e) {
-  if (e.data.d === "n") {
-    currdeg = currdeg - 45;
-    $(".text" + num).fadeOut();
-    if (num === 8) {
-      num = 1;
-    } else {
-      num = num + 1;
+    if (e.data.d == 'n') {
+        currdeg = currdeg - 45;
+        $('.text' + num).fadeOut();
+        if (num == 8) {
+            num = 1;
+        } else {
+            num = num + 1;
+        }
+        $('.text' + num).fadeIn();
     }
-    $(".text" + num).fadeIn();
-  }
-  if (e.data.d === "p") {
-    currdeg = currdeg + 45;
-    $(".text" + num).fadeOut();
-    if (num === 1) {
-      num = 8;
-    } else {
-      num = num - 1;
+    if (e.data.d == 'p') {
+        currdeg = currdeg + 45;
+        $('.text' + num).fadeOut();
+        if (num == 1) {
+            num = 8;
+        } else {
+            num = num - 1;
+        }
+        $('.text' + num).fadeIn();
     }
-    $(".text" + num).fadeIn();
-  }
-  carousel.css({
-    "-webkit-transform": "rotateY(" + currdeg + "deg)",
-    "-moz-transform": "rotateY(" + currdeg + "deg)",
-    "-o-transform": "rotateY(" + currdeg + "deg)",
-    transform: "rotateY(" + currdeg + "deg)",
-  });
+    carousel.css({
+        '-webkit-transform': 'rotateY(' + currdeg + 'deg)',
+        '-moz-transform': 'rotateY(' + currdeg + 'deg)',
+        '-o-transform': 'rotateY(' + currdeg + 'deg)',
+        transform: 'rotateY(' + currdeg + 'deg)',
+    });
 }
